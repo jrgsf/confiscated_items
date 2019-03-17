8048e7c9c4205ab15694ab3bfa2f4426cec05f97
Casey Gorman <caseyagorman@gmail.com>
2019-03-17 15:10:01 -0700

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import Test from './testytest.js'
-import EntryForm from './EntryForm.js'
-
+import React, { Component } from "react";
+import Test from "./testytest.js";
+import EntryForm from "./EntryForm.js";
+// test comment
 class App extends Component {
-// constructor(props){
-//     super(props)
-//     this.state = {thing: ""}
-// }
-//
-// componentDidMount(){
-//   this.setState({thing: "some other dang state"});
-// }
-//
-// displayComponent(putting_thing_here) {
-//   console.log("first log check", putting_thing_here)
-//   if (!putting_thing_here){
-//     return <div>That not here solly</div>
-//   }
-//   return <Test thing={putting_thing_here} />;
-// }
+  // constructor(props){
+  //     super(props)
+  //     this.state = {thing: ""}
+  // }
+  //
+  // componentDidMount(){
+  //   this.setState({thing: "some other dang state"});
+  // }
+  //
+  // displayComponent(putting_thing_here) {
+  //   console.log("first log check", putting_thing_here)
+  //   if (!putting_thing_here){
+  //     return <div>That not here solly</div>
+  //   }
+  //   return <Test thing={putting_thing_here} />;
+  // }
 
   render() {
-    console.log("check render")
-    return <EntryForm/>
+    console.log("check render");
+    return <EntryForm />;
   }
 }
 
